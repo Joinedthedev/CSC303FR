6465c61f5e7b660b3a7d723eb9166e1a8cb0f5e9
Salim <salimbabaji@gmail.com>
2022-11-07 12:40:16 +0100

    Bro i actually have no idea what im doing here. Java is kinda weird. I miss js and my csharp BAYBEEEE

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import javax.xml.namespace.QName;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       /* Scanner sc= new Scanner(System.in);
+
+        System.out.print("Add your first number");
+
+        int x = sc.nextInt();
+
+        System.out.print("Add your second number");
+
+        int y = sc.nextInt();
+
+        System.out.print("Add your third number");
+
+        int z = sc.nextInt();
+
+        int zz = x+y+z;
+
+        System.out.println(zz);
+        NAME();*/
+
+        do_while();
+        do_while2();
     }
+
+    public static void NAME(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name and classes");
+        String name = sc.nextLine();
+        String Class1= sc.nextLine();
+        String Class2= sc.nextLine();
+        String Class3= sc.nextLine();
+
+        System.out.println("My name is " + name + " I have the following classes:" +"\t\n" + Class1 + "\n" + Class2 + "\n" + Class3 );
+
+
+    }
+
+        public static void do_while(){
+        int num = 1;
+        int sum = 0;
+
+        do{
+            sum+= num;
+            num++;
+
+        }while(num<=100);{
+            System.out.println(sum);
+            }
+
+
+    }
+
+    public static void do_while2(){
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<5);
+    }
+
+
 }
